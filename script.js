9d8e8d0a675dec98711cb5ba76a63e7652c7dd1d
mar-roman <roman.markaryants@yandex.ru>
2021-08-05 14:37:18 +0300

    confirm difficult task

--- script.js
+++ script.js
@@ -3,42 +3,57 @@
 //Запускает программу
 function start() {
 
-  //Заканчивает игру
-  function finish(){}
+  //Заканчивает игру. Пустая функция.
+  function finish() {
+    alert('Игра окончена');
+  }
 
   //Генерация случайного числа от 1 до 100
   let randomNumber = Math.round(Math.random()*100); 
   console.log(randomNumber);
   let num = +prompt('Угадай число от 1 до 100');
+  //Количество попыток
+  let k = 10;
 
-  //Проверка на число
-  function isNumber() {
-    if (num === 0) {
-      alert('Игра окончена');
-    } else if (isNaN(parseFloat(num) && isFinite(num))) {
+  //Новая попытка
+  function attempt() {
+    if (isNaN(parseFloat(num) && isFinite(num)) && (num > 0)) {
       num = +prompt('Введи число!');
-      isNumber(num);
+      attempt(num);
     }
   }
 
-  isNumber();
+  attempt();
 
-  //Сравнивает число со сгенерированным
+  //Сравнивает число со сгенерированным и изменяет количество попыток
   function compair() {
     if (num === randomNumber){
-      alert('Поздравляю, Вы угадали!!!');
+      let again = confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?');
+      if(again){
+        start();
+      } else {
+        finish();
+      }
     } else if(num === 0) {
       finish();
     } else {
-      //Новая попытка угадать число
-      if (num > randomNumber){
-        num = +prompt('Загаданное число меньше. Попробуй отгадать снова');
-        isNumber();
-        compair();
+      k--;
+      if(k === 0){
+        let again = confirm('Попытки закончились, хотите сыграть еще?');
+        if(again){
+          start();
+        }
       } else {
-        num = +prompt('Загаданное число больше. Попробуй отгадать снова');
-        isNumber();
-        compair();
+        //Новая попытка угадать число
+        if (num > randomNumber){
+          num = +prompt('Загаданное число меньше, осталось попыток ' + k + ' Попробуй отгадать снова');
+          attempt();
+          compair();
+        } else {
+          num = +prompt('Загаданное число больше, осталось попыток ' + k + ' Попробуй отгадать снова');
+          attempt();
+          compair();
+        }
       }
     }
   }
